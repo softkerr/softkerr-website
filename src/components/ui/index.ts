59d6e76f6881d89c7b3d428a8c648dbba920c782
bakryu <stasbakryu@gmail.com>
2024-11-14 11:00:00 +0200

    Create UI index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,28 @@
+// Form Components
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Select } from './Select';
+export { default as Textarea } from './Textarea';
+
+// Layout Components
+export { default as Container } from './Container';
+export { default as Section } from './Section';
+export { default as Card } from './Card';
+
+// Typography & Navigation
+export { default as Typography } from './Typography';
+export { default as Link } from './Link';
+
+// Existing Components
+export { default as ScrollReveal } from './ScrollReveal';
+
+// Export types
+export type { ButtonProps } from './Button';
+export type { InputProps } from './Input';
+export type { SelectProps, SelectOption } from './Select';
+export type { TextareaProps } from './Textarea';
+export type { ContainerProps } from './Container';
+export type { SectionProps } from './Section';
+export type { CardProps } from './Card';
+export type { TypographyProps } from './Typography';
+export type { LinkProps } from './Link';
